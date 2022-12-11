7c5f314cc91cd0ea963a13327c095a3f6f3208d2
Martin Zimmermann <martinzimmermann@outlook.de>
2022-12-11 20:09:53 +0100

    Cleaned up Day06

--- Day06/tuning_trouble.py
+++ Day06/tuning_trouble.py
@@ -3,20 +3,18 @@ import os.path
 input_file = os.path.dirname(os.path.realpath(__file__)) + "/ExampleInput.txt"
 input_file = os.path.dirname(os.path.realpath(__file__)) + "/MyInput.txt"
 
-
 def dupcheck(x):
     for elem in x:
         if x.count(elem) > 1:
             return True    
     return False
 
+#Part1
 with open(input_file) as data_set:
-    message = data_set.readline()
-    print(message)
+    message = data_set.readline()    
     
     for i in range(len(message)):
         package = message[i:(i+4)]
-        
         if False == dupcheck(package):
             print("Solution of Part 1:", i + 4)
             break
@@ -24,11 +22,9 @@ with open(input_file) as data_set:
 #Part 2
 with open(input_file) as data_set:
     message = data_set.readline()
-    print(message)
-    
+
     for i in range(len(message)):
         package = message[i:(i+14)]
-        
         if False == dupcheck(package):
-            print("Solution of Part 1:", i + 14)
+            print("Solution of Part 2:", i + 14)
             break
